51a943518e454d07687363004c2c9c3c939f1ed6
sssgordon <gso7@uwo.ca>
2020-01-21 16:11:12 +0100

    Style menu items

--- main.js
+++ main.js
@@ -4,18 +4,26 @@ $(".menu-button").click(function() {
         gsap.to(".hamburger-top", {
             y: 0,
             rotation: 0,
+            backgroundColor: "white",
             duration: 0.2
         });
         gsap.to(".hamburger-bottom", {
             y: 0,
             rotation: 0,
+            backgroundColor: "white",
             duration: 0.2
         });
     } else {
-        gsap.to(".hamburger-top", { y: "0.6rem", rotation: 45, duration: 0.2 });
+        gsap.to(".hamburger-top", {
+            y: "0.6rem",
+            rotation: 45,
+            backgroundColor: "black",
+            duration: 0.2
+        });
         gsap.to(".hamburger-bottom", {
             y: "-0.6rem",
             rotation: -45,
+            backgroundColor: "black",
             duration: 0.2
         });
     }
